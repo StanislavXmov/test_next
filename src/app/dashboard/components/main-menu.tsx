3fb56835289c793eb56d803e08936b2c0364cfd7
stanislav <xiiiovart@gmail.com>
2024-07-29 12:30:54 +0300

    main menu footer

--- src/app/dashboard/components/main-menu.tsx
+++ src/app/dashboard/components/main-menu.tsx
@@ -1,13 +1,16 @@
+import { Avatar, AvatarFallback } from "@/shared/ui/avatar";
 import MenuItem from "./menu-item";
 import MenuTitle from "./menu-title";
+import Link from "next/link";
+import { LightDarkToogle } from "@/shared/ui/light-dark-toggle";
 
 export default function MainMenu() {
   return (
-    <div className="bg-muted overflow-auto p-4">
+    <div className="bg-muted overflow-auto p-4 flex flex-col">
       <div className="border-b dark:border-b-black border-b-zinc-300 pb-4">
         <MenuTitle />
       </div>
-      <div className="py-4">
+      <div className="py-4 grow">
         <MenuItem href="/dashboard">
           My dashboard
         </MenuItem>
@@ -23,6 +26,17 @@ export default function MainMenu() {
         <MenuItem href="/dashboard/settings">
           Settings
         </MenuItem>
+      </div>
+      <div className="flex gap-2 items-center">
+        <Avatar>
+          <AvatarFallback className="bg-primary">
+            US
+          </AvatarFallback>
+        </Avatar>
+        <Link href={'/'} className="hover:underline" >
+          Logout
+        </Link>
+        <LightDarkToogle className="ml-auto" />
       </div>
     </div>
   );
