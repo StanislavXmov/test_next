f47c3d55729a942d7ae0a2d089e29a172c5c8ed4
stanislav <xiiiovart@gmail.com>
2024-07-29 19:19:04 +0300

    add tabs

---
+++ src/app/dashboard/components/employees-stats.tsx
@@ -0,0 +1,17 @@
+import { Card } from "@/shared/ui/card";
+
+export default function EmployessStats() {
+  return (
+    <div className="grid lg:grid-cols-3 gap-4">
+      <Card>
+        Card 1
+      </Card>
+      <Card>
+        Card 2
+      </Card>
+      <Card>
+        Card 3
+      </Card>
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,23 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/shared/ui/tabs";
+import EmployessStats from "./components/employees-stats";
+
 export default function DashboardPage() {
   return (
-    <div>
-      DashboardPage
-    </div>
+    <Tabs defaultValue="employees">
+      <TabsList className="mb-4">
+        <TabsTrigger value="employees">
+          Employess stats
+        </TabsTrigger>
+        <TabsTrigger value="teams">
+          Teams stats
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="employees">
+        <EmployessStats />
+      </TabsContent>
+      <TabsContent value="teams">
+        Teams stats
+      </TabsContent>
+    </Tabs>
   );
 }
